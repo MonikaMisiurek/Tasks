c159cd3b0d59ca8343f0cea554047543a339c835
Monia <monika.misiurek01@gmail.com>
2022-09-29 10:55:00 +0200

    jdbc zadania

---
+++ src/main/java/jdbctest/task1/Task1.java
@@ -0,0 +1,22 @@
+package jdbctest.task1;
+/*
+Wykorzystując JDBC API oraz dowolną relacyjną bazę danych (np.H2) wykonaj następujące zapytania:
+    • stwórz tabelę MOVIES z kolumnami: id typu INTEGER AUTO INCREMENT,title typu VARCHAR(255),
+     genre typu VARCHAR(255), yearOfRelease typu INTEGER. Weź pod uwagę, że tabela o nazwie MOVIE może
+    już istnieć. W takim przypadku usuń ją.
+    • dodaj trzy dowolne rekordy do tabeli MOVIES
+    • zaktualizuj jeden wybrany rekord (skorzystaj z PreparedStatement)
+    • usuń wybrany rekord o określonym identyfikatorze
+    • wyświetl wszystkie pozostałe rekordy w bazie
+W zadaniu skup się na poprawnym wykorzystaniu JDBC API. Wszystkie zapytania mogą być wykonane bezpośrednio w
+metodzie main. Wykorzystaj pojedyncze połączenie do wykonania wszystkich zapytań. Pamiętaj jednak o
+ wykorzystaniu try-with-resources podczas otwierania połączenia oraz tworzenia obiektów
+ typu Statement czy PreparedStatement. W zadaniu tym również nie przejmuj się
+obsługą wyjątków (w przypadku błędu, wyświetl stacktrace).
+ */
+
+public class Task1 {
+
+
+
+}
